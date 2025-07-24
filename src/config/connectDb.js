78347d8993c54f89eb791deae104d66b3fa8dc67
quangluongw <quang20042204@gmail.com>
2025-07-24 22:45:46 +0700

    fix connectdb

--- src/config/connectDb.js
+++ src/config/connectDb.js
@@ -3,7 +3,7 @@ export const connectDb = async () => {
   try {
     await mongoose
       .connect(
-        "mongodb+srv://quang20042204:G2ewkblkJnYnV1IY@cluster0.m5sifsi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+        "mongodb+srv://quang20042204:sJnNlNJBsdpmGbTD@cluster0.xk6mqgw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
       )
       .then();
     
